4b32f01015eebb9c4fcf41928c197344953b81ad
Tikovka72 <zeytunyan2004@mail.ru>
2020-10-17 19:20:11 +0500

    logging all errors

--- core.py
+++ core.py
@@ -2,6 +2,7 @@ from vk_api import VkApi
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 import logging
+import sys
 
 from constants import TOKEN, SQL_FILE_NAME
 from events_reactions import *
@@ -24,23 +25,28 @@ def main() -> None:
     upload = vk_api.VkUpload(vk_session)
     vk = vk_session.get_api()
     while True:
-        try:
-            for event in longpoll.listen():
-                if event.type == VkBotEventType.MESSAGE_NEW:
-                    log = u"{} IN {}".format(event.obj.message["from_id"],
-                                             event.chat_id or event.obj.message["from_id"])
-                    logging.info(log)
-                    new_message_reaction(event, vk, sqlite, upload)
-
-                if event.type == VkBotEventType.MESSAGE_ALLOW:
-                    allow_messages_reaction(event, vk, sqlite)
-                if event.type == VkBotEventType.MESSAGE_REPLY:
-                    log = u"ANSWER ON {}".format(event.obj["peer_id"])
-                    logging.info(log)
-        except ValueError as e:
-            logging.error(repr(e))
-            send_message(f"{repr(e)}", vk, int(CHIEF_ADMIN))
+        for event in longpoll.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                log = u"{} IN {}".format(event.obj.message["from_id"],
+                                         event.chat_id or event.obj.message["from_id"])
+                logging.info(log)
+                new_message_reaction(event, vk, sqlite, upload)
+            if event.type == VkBotEventType.MESSAGE_ALLOW:
+                allow_messages_reaction(event, vk, sqlite)
+            if event.type == VkBotEventType.MESSAGE_REPLY:
+                log = u"ANSWER ON {}".format(event.obj["peer_id"])
+                logging.info(log)
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        try:
+            main()
+        except Exception as e:
+            try:
+                type_ex, obj_ex, info = sys.exc_info()
+                file = info.tb_frame.f_code.co_filename
+                line = info.tb_lineno
+                logging.error(f"{type_ex} | msg: {obj_ex} | line: {file} | line: {line}")
+            except Exception:
+                pass
