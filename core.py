42a64ba449860b727bac4c9077e97c256ab483fa
Tikovka72 <zeytunyan2004@mail.ru>
2020-10-17 23:11:56 +0500

    log fix

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 import logging
 import sys
+from transliterate import translit
 
 from constants import TOKEN, SQL_FILE_NAME
 from events_reactions import *
@@ -27,14 +28,17 @@ def main() -> None:
     while True:
         for event in longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
-                log = u"{} IN {}".format(event.obj.message["from_id"],
-                                         event.chat_id or event.obj.message["from_id"])
+                log = u"{} IN {}: {}".format(event.obj.message["from_id"],
+                                         event.chat_id or event.obj.message["from_id"],
+                                             translit(event.obj.message["text"],
+                                                      "ru", reversed=True))
                 logging.info(log)
                 new_message_reaction(event, vk, sqlite, upload)
             if event.type == VkBotEventType.MESSAGE_ALLOW:
                 allow_messages_reaction(event, vk, sqlite)
             if event.type == VkBotEventType.MESSAGE_REPLY:
-                log = u"ANSWER ON {}".format(event.obj["peer_id"])
+                log = u"ANSWER {}: {}".format(event.obj["peer_id"], translit(event.obj.text, "ru",
+                                                                                reversed=True))
                 logging.info(log)
 
 
@@ -45,8 +49,16 @@ if __name__ == '__main__':
         except Exception as e:
             try:
                 type_ex, obj_ex, info = sys.exc_info()
-                file = info.tb_frame.f_code.co_filename
                 line = info.tb_lineno
-                logging.error(f"{type_ex} | msg: {obj_ex} | line: {file} | line: {line}")
-            except Exception:
+                file = info.tb_frame.f_code.co_filename
+                while True:
+                    info = info.tb_next
+                    if info:
+                        line = info.tb_lineno
+                        file = info.tb_frame.f_code.co_filename
+                    else:
+                        break
+                logging.error(f"{type_ex} | msg: {translit(str(obj_ex), 'ru', reversed=True)}"
+                              f" | file: {file} | line: {line}")
+            except Exception as e:
                 pass
