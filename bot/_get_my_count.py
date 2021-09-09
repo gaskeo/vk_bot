e3e35f5934da6532ea79a3be0afc8f742dce75a1
Tikovka72 <zeytunyan2004@mail.ru>
2021-09-09 19:45:36 +0500

    hot fix

--- bot/_get_my_count.py
+++ bot/_get_my_count.py
@@ -1,6 +1,7 @@
-from utils import send_message
+from utils import send_message, get_user_name
 
 
 def get_my_count(self, event, _, peer_id):
-    count = self.redis.get_count_messages(str(peer_id), str(event.obj.message["from_id"]))
-    send_message(f"вы написали сообщений: {count}", self.vk, peer_id)
+    user_id = event.obj.message['from_id']
+    count = self.redis.get_count_messages(str(peer_id), str(user_id))
+    send_message(f"{'@id'}{user_id} ({get_user_name(user_id, self.vk)}): {count}", self.vk, peer_id)
