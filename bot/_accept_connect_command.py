b367bc10bff8fb6b8ce8d6c942a933496421b647
Tikovka72 <zeytunyan2004@mail.ru>
2021-09-17 21:36:55 +0500

    refactor 'accept connection'

--- bot/_accept_connect_command.py
+++ bot/_accept_connect_command.py
@@ -4,16 +4,21 @@ from constants import MIN_CHAT_PEER_ID
 from my_vk_api import get_admins_in_chat
 
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from . import Bot
 
 
 def accept_connect(self: 'Bot', event: bot_longpoll.VkBotEvent, _, peer_id: int):
-    print(type(event))
-    if peer_id > MIN_CHAT_PEER_ID:
-        admins = get_admins_in_chat(peer_id, self.vk)
-        if event.obj["user_id"] in admins:
-            self.redis.connect(str(peer_id), event.obj["payload"]["peer_id"])
-            self.redis.delete_token(event.obj["payload"]["token"])
-            self.send_message("вы подключились", str(peer_id))
-            self.send_message("вы подключились", event.obj["payload"]["peer_id"])
+    if peer_id < MIN_CHAT_PEER_ID:
+        return
+
+    admins = get_admins_in_chat(peer_id, self.vk)
+    if event.obj["user_id"] not in admins:
+        return
+
+    self.redis.connect(str(peer_id), event.obj["payload"]["peer_id"])
+    self.redis.delete_token(event.obj["payload"]["token"])
+
+    self.send_message("вы подключились", str(peer_id))
+    self.send_message("вы подключились", event.obj["payload"]["peer_id"])
