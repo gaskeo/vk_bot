c66375f255ef174b533f8a93f9e720e1872d514d
Tikovka72 <zeytunyan2004@mail.ru>
2021-09-17 21:37:41 +0500

    refactor 'admin help'

--- bot/_admin_help_command.py
+++ bot/_admin_help_command.py
@@ -1,15 +1,16 @@
 from constants import MIN_CHAT_PEER_ID, ADMIN_TEXT
 
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from . import Bot
 
 
 def admin_help(self: 'Bot', _, __, peer_id: int):
-    if not peer_id > MIN_CHAT_PEER_ID:
+    if peer_id > MIN_CHAT_PEER_ID:
+        return
 
-        level: int = self.redis.get_admin(str(peer_id))
-        if level > 0:
-            self.send_message(ADMIN_TEXT, str(peer_id))
-        else:
-            self.send_message("У вас нет доступа к данной команде", str(peer_id))
+    level: int = self.redis.get_admin(str(peer_id))
+
+    if level > 0:
+        self.send_message(ADMIN_TEXT, str(peer_id))
