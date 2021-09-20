f517412777ada530bc6527224dd7f5fe03156cb2
Tikovka72 <zeytunyan2004@mail.ru>
2021-09-20 23:43:40 +0500

    small refactor in commands

--- bot/_get_words_after_that_command.py
+++ bot/_get_words_after_that_command.py
@@ -10,18 +10,10 @@ def get_words_after_that(self: 'Bot', event: bot_longpoll.VkBotMessageEvent, mes
         if "///end" in words:
             words.remove("///end")
         if not words:
-            self.send_message("ничего нет", str(peer_id))
-            return
+            return self.send_message("ничего нет", str(peer_id))
         self.send_message((f"после {message} идет:\n" +
                            "\n".join(("- " + i for i in words))), str(peer_id), reply_to=event.obj.message.get("id"))
 
-    message = message.replace("/at", "").strip()
-    if not message or message == " ":
-        if event.obj.message.get("reply_message", False):
-            message = event.obj.message["reply_message"]["text"]
-            if len(message.split()) > 0:
-                words = list(self.redis.get_words_after_that(str(peer_id), message).keys())
-                send_ans()
-                return
-    words = list(self.redis.get_words_after_that(str(peer_id), message).keys())
-    send_ans()
+    if len(message) > 0:
+        words = list(self.redis.get_words_after_that(str(peer_id), message).keys())
+        return send_ans()
